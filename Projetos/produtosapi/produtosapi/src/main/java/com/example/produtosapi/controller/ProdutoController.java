5a3bae6aee7f93c0f52d9ef450faf6d5ecd3ea28
unknown <dev32@NB-UNIAGI-32.AGIPLAN.LOCAL>
2025-09-12 11:27:16 -0300

    Feat: Adicionando funcionalidades pro projeto

--- Projetos/produtosapi/produtosapi/src/main/java/com/example/produtosapi/controller/ProdutoController.java
+++ Projetos/produtosapi/produtosapi/src/main/java/com/example/produtosapi/controller/ProdutoController.java
@@ -2,10 +2,7 @@ package com.example.produtosapi.controller;
 
 import com.example.produtosapi.model.Produto;
 import com.example.produtosapi.repository.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -21,12 +18,22 @@ public class ProdutoController {
     }
 
     @PostMapping
-    public void salvar(@RequestBody Produto produto){
+    public Produto salvar(@RequestBody Produto produto){
 
         var id = UUID.randomUUID().toString();
         produto.setId(id);
 
         produtoRepository.save(produto);
+        return produto;
     }
 
+    @GetMapping("/{id}")
+    public Produto buscarPorId(@PathVariable("id") String id){
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletarPorId(@PathVariable("id") String id){
+        produtoRepository.deleteById(id);
+    }
 }
